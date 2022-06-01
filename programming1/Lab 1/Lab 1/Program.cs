82de5a39fa7fb71a4aacc8bc50fb23799f90b4f5
baylenjones <baylen.jones0224@gmail.com>
2022-05-31 21:08:39 -0500

    first commit

---
+++ programming1/Lab 1/Lab 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Full Sail!");
+            Console.WriteLine("what is your name? ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("What is your age? ");
+            string? age = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}. Congratulations on living to the age of {age}!");
+            Console.ReadKey();
+
+        }
+    }
+}
