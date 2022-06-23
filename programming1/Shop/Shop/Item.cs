697279c0ca2382d5d63287b1177a236bb183d213
baylenjones <baylen.jones0224@gmail.com>
2022-06-22 23:17:33 -0500

    20220622

---
+++ programming1/Shop/Shop/Item.cs
@@ -0,0 +1,58 @@
+﻿//=============================================================================
+// Program's Item class
+//=============================================================================
+
+namespace Shop
+{
+    // TODO: Write a class called Item that contains 2 data members:
+    //       A string called name and an int called cost.
+    //       This class should have a default constructor that sets name to ""
+    //       and cost to 0.
+    //       It should also have an overloaded constructor that accepts 2
+    //       parameters - a string and an int.
+    //
+    //       Write getters/accessors for each data member. They should be called
+    //       GetName and GetCost.
+    //
+    //       Write setters/mutators for each data member. They should be called
+    //       SetName and SetCost.
+
+    public class Item
+    {
+        string name;
+        int cost;
+
+        public Item()
+        {
+            this.name = "";
+            this.cost = 0;
+        }
+
+        public Item(string name, int cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public int GetCost()
+        {
+            return this.cost;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetCost(int cost)
+        {
+            this.cost = cost;
+        }
+    }
+
+}
